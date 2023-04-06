e9acbedd7f913007f8ed71f57fffdf006ecf8c7c
roy kimathi <kimathiwaweru@gmail.com>
2023-04-06 08:31:17 +0300

    node index.js tests are passing

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-  const lenght = word.length;
-  for (let j=0; j<lenght/2; j++){
-    if (word[j]===word[lenght-1-j]){  
-      return true;
-    }else {
-      return false;
-    }
-  }
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
 */
+// reverse the input string
+
+// if the reversed string is the same as the input
+//   return true
+// else
+//   return false
 
 /*
   Add written explanation of your solution here
@@ -23,6 +27,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
